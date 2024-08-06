c5f7e0245392338c3b081492094336dc10008206
Luis Fernando <guffenix@gmail.com>
2024-08-05 23:12:25 -0500

    feat: improve deploy script

--- script/deploy.js
+++ script/deploy.js
@@ -1,21 +1,27 @@
-const { ethers } = require('hardhat')
+const { ethers, deployments } = require('hardhat')
 
-module.exports = async ({ deployments }) => {
+module.exports = async () => {
   const { deploy } = deployments
-  const { deployer } = await ethers.getSigners()
+  const signers = await ethers.getSigners()
+
+  const deployer = signers[0]
 
   console.log('Deploying contracts with the account:', deployer.address)
 
-  const deploymentResult = await deploy('RetroMomentRoll', {
-    from: deployer.address,
-    args: [deployer.address],
-    log: true,
-  })
+  try {
+    const deploymentResult = await deploy('RetroMomentRoll', {
+      from: deployer.address,
+      args: [deployer.address],
+      log: true,
+    })
 
-  if (deploymentResult.newlyDeployed) {
-    console.log(
-      `Contract RetroMomentRoll deployed at ${deploymentResult.address} using ${deploymentResult.receipt.gasUsed} gas`
-    )
+    if (deploymentResult.newlyDeployed) {
+      console.log(
+        `Contract RetroMomentRoll deployed at ${deploymentResult.address} using ${deploymentResult.receipt.gasUsed} gas`
+      )
+    }
+  } catch (error) {
+    console.error('Error deploying contract:', error)
   }
 }
 
