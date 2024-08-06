074bd98240683ea8e2788338a9c23fd8988a3b54
Luis Fernando <guffenix@gmail.com>
2024-08-05 23:13:15 -0500

    fix: initial owner param added

--- ignition/modules/RetroMomentRoll.js
+++ ignition/modules/RetroMomentRoll.js
@@ -1,7 +1,7 @@
 const { buildModule } = require('@nomicfoundation/hardhat-ignition/modules')
 
 module.exports = buildModule('RetroMomentRollModule', (m) => {
-  const initialOwner = m.getParameter('initialOwner')
+  const initialOwner = m.getParameter('initialOwner', process.env.INITIAL_OWNER)
 
   const retroMomentRoll = m.contract('RetroMomentRoll', [initialOwner])
 
